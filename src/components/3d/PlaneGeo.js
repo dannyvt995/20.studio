fc484f37aca84bee5a2d7ee22de490a5348c51eb
tuananhnvl <tuananh.namvietluat@gmail.com>
2023-03-14 18:24:12 +0700

    up

--- src/components/3d/PlaneGeo.js
+++ src/components/3d/PlaneGeo.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useState } from "react";
 import { useThree, useLoader } from "@react-three/fiber";
 import * as THREE from 'three';
-import gsap,{Power2 } from "gsap";
+import gsap, { Power2 } from "gsap";
 const images = {
     image1: require('../.././asset/gallery/3.png'),
     image2: require('../.././asset/gallery/5.png'),
@@ -13,27 +13,72 @@ const images = {
 export default function PlaneGeo() {
     const [targets, setTargets] = useState([]);
     const [targetHeight, setTargetHeight] = useState([]);
+    const [calcY, setCalcY] = useState();
     const [targetsCompare, setTargetsCompare] = useState([]);
     const { viewport, scene, mouse } = useThree();
     const meshRef = useRef();
     const [dragging, setDragging] = useState(false);
     const [dragOffset, setDragOffset] = useState({ x: 0, y: 0 });
     const W_GEO = 80
-   
 
+    const heightRandom = [
+        110,
+        70,
+        110,
+        110,
+        160,
+        70,
+        160,
+        110,
+        160,
+        110,
+        70,
+        70,
+        70,
+        110,
+        160,
+        110,
+        160,
+        160,
+        160,
+        110,
+        70,
+        160,
+        70,
+        110,
+        110,
+        160,
+        70,
+        70,
+        110,
+        70,
+        70,
+        110,
+        160,
+        70,
+        70,
+        160,
+        110,
+        160,
+        70,
+        110,
+        160,
+        70,
+        110,
+        110,
+        70,
+        160,
+        70,
+        70,
+        70,
+        70
+    ]
     const COL_GEO = 10
     const ROW_GEO = 5
-    const tex = [
-        useLoader(THREE.TextureLoader, images.image1),
-        useLoader(THREE.TextureLoader, images.image2),
-        useLoader(THREE.TextureLoader, images.image3),
-        useLoader(THREE.TextureLoader, images.image4),
-        useLoader(THREE.TextureLoader, images.image5)
-    ];
 
 
     const [targetsPos, setTargetsPos] = useState([]);
-    
+
 
     const onPointerDown = (e) => {
         console.log(`${Math.round(mouse.x * 100) / 100} === mouseX , ${Math.round(mouse.y * 100) / 100} === mouseY`)
@@ -58,33 +103,34 @@ export default function PlaneGeo() {
 
         }
     };
-   
+
     useEffect(() => {
-        meshRef.current.position.set(-450,-200, 0)
-       
+        meshRef.current.position.set(-450, -300, 0)
+
     }, [meshRef])
-   
+
 
     useEffect(() => {
 
         let H_GEO = 150
         let l = Math.random(150) + 20
-      
-        for (let i = 0; i < COL_GEO ; i++) {
+
+        for (let i = 0; i < COL_GEO; i++) {
             for (let j = 0; j < ROW_GEO; j++) {
                 let u = Math.random(100) + 10
                 let p = (u - 10) * 100
 
                 //let h = Math.floor(Math.random() * 100) + 50;
-                
-                let arrh = [70,110,160]
+
+                let arrh = [70, 110, 160]
                 let m = Math.floor(Math.random() * 3)
                 let h = arrh[m]
                 console.log(h)
-                setTargetsPos(targetsPos => [...targetsPos,[(W_GEO + 20) * i ,h * j, 0]] ); 
-                setTargetHeight(targetHeight => [...targetHeight,h])
+                setTargetsPos(targetsPos => [...targetsPos, [(W_GEO + 20) * i, 100 * j, 0]]);
+                setTargetHeight(targetHeight => [...targetHeight, h])
+             
 
-                let texture =  new THREE.TextureLoader().load(images[`image${Math.floor(Math.random() * 5 + 1)}`])
+                let texture = new THREE.TextureLoader().load(images[`image${Math.floor(Math.random() * 5 + 1)}`])
                 let aspectOfPlane = W_GEO / h;
                 let aspectOfImage = 644 / 965;
                 let yScale = 1;
@@ -94,93 +140,124 @@ export default function PlaneGeo() {
                     yScale = aspectOfImage / aspectOfPlane;
                 }
                 texture.repeat.set(xScale, yScale);
-                texture.offset.set((1 - xScale) / 2, (1 - yScale) / 2);  
-            
-               // console.log(texture)
-            
+                texture.offset.set((1 - xScale) / 2, (1 - yScale) / 2);
+
+                // console.log(texture)
+
 
                 targets.push(
-                <mesh key={Math.random(50) + j}
-                    position={[0, 0, -420]} // Pos defuat
-                    scale = {[.42,.42,.42]}
-                    onPointerDown={onPointerDown}
-                    onPointerMove={onPointerMove}
-                    onPointerUp={onPointerUp}>
-                    <planeGeometry args={[W_GEO,h]} />
-                    <meshBasicMaterial transparent  opacity={0}  map={texture} />
-                </mesh>
+                    <mesh key={Math.random(50) + j}
+                        position={[0, 0, -420]} // Pos defuat
+                        scale={[.42, .42, .42]}
+                        onPointerDown={onPointerDown}
+                        onPointerMove={onPointerMove}
+                        onPointerUp={onPointerUp}>
+                        <planeGeometry args={[W_GEO, h]} />
+                        <meshBasicMaterial transparent opacity={0} map={texture} />
+                    </mesh>
                 )
-                
-    
-               
+
+
+
 
 
             }
         }
-      
+
     }, [scene]);
 
     useEffect(() => {
-        if(targetsPos.length > ((COL_GEO * ROW_GEO)/3*2)) {
+        if (targetsPos.length > ((COL_GEO * ROW_GEO) / 3 * 2)) {
             //console.log(targetsPos[1][1])
             console.log(targetHeight)
             console.log(targetsPos)
-           /*  console.log(targetHeight[0])
-            console.log(targetsPos[0][1])
-            console.log(targetHeight[1])
-            console.log(targetsPos[1][1])
-            console.log(targetHeight[5])
-            console.log(targetsPos[5][1]) */
+     
             setTimeout(() => {
                 let a = meshRef.current.children
+
+                for (let e = 0; e < a.length; e+=5) {
+                        let delayT = Math.random(1) + 0.1
+                        gsap.to(a[e].position, {
+                            x: targetsPos[e][0],
+                            y: targetsPos[e][1]  + (targetHeight[e] / 2),
+                            z: targetsPos[e][2],
+                            delay: delayT,
+                            duration: 1,
+                            ease: Power2.easeInOut
+                        })
+                        animaChild(a[e], delayT)
               
-                for (let e = 0; e < 10; e++) {
-                    if(e == 0 || e == 5 || e == 10 || e == 15 || e == 20 || e == 25 || e == 30 || e == 35 || e == 40 || e == 45) {
-                        let delayT = Math.random(1) + 0.1 
-                        gsap.to(a[e].position,{
-                            x :targetsPos[e][0],
-                            y: targetsPos[e][1] + (targetHeight[e] / 2),
-                            z : targetsPos[e][2],
-                            delay: delayT,
-                            duration: 1,
-                            ease:Power2.easeInOut
-                        })
-                        animaChild(a[e],delayT)
-                    }else{
-                        let delayT = Math.random(1) + 0.1 
-                        gsap.to(a[e].position,{
-                            x :targetsPos[e][0],
-                            y: targetsPos[e][1] + targetHeight[e],
-                            z : targetsPos[e][2],
-                            delay: delayT,
-                            duration: 1,
-                            ease:Power2.easeInOut
-                        })
-                        animaChild(a[e],delayT)
-                    }
-                    
-                    
+
                 }
+                for (let e = 1; e < a.length; e+=5) {
+                    let delayT = Math.random(1) + 0.1
+                    gsap.to(a[e].position, {
+                        x: targetsPos[e][0],
+                        y: targetHeight[e - 1] + (targetHeight[e] / 2) + 20,
+                        z: targetsPos[e][2],
+                        delay: delayT,
+                        duration: 1,
+                        ease: Power2.easeInOut
+                    })
+                    animaChild(a[e], delayT)
+                }
+                for (let e = 2; e < a.length; e+=5) {
+                    let delayT = Math.random(1) + 0.1
+                    gsap.to(a[e].position, {
+                        x: targetsPos[e][0],
+                        y: targetHeight[e - 2] + targetHeight[e - 1] + (targetHeight[e] / 2) + 40,
+                        z: targetsPos[e][2],
+                        delay: delayT,
+                        duration: 1,
+                        ease: Power2.easeInOut
+                    })
+                    animaChild(a[e], delayT)
+                }
+                for (let e = 3; e < a.length; e+=5) {
+                    let delayT = Math.random(1) + 0.1
+                    gsap.to(a[e].position, {
+                        x: targetsPos[e][0],
+                        y: targetHeight[e - 3] + targetHeight[e - 2] + targetHeight[e - 1] + (targetHeight[e] / 2) + 60,
+                        z: targetsPos[e][2],
+                        delay: delayT,
+                        duration: 1,
+                        ease: Power2.easeInOut
+                    })
+                    animaChild(a[e], delayT)
+                }
+                for (let e = 4; e < a.length; e+=5) {
+                    let delayT = Math.random(1) + 0.1
+                    gsap.to(a[e].position, {
+                        x: targetsPos[e][0],
+                        y: targetHeight[e - 4] + targetHeight[e - 3] + targetHeight[e - 2] + targetHeight[e - 1] + (targetHeight[e] / 2) + 80,
+                        z: targetsPos[e][2],
+                        delay: delayT,
+                        duration: 1,
+                        ease: Power2.easeInOut
+                    })
+                    animaChild(a[e], delayT)
+                }
+               
             }, 1000);
         }
-      
-    },[targetsPos,meshRef])
 
-    function animaChild(target,delay) {
-        gsap.to(target.scale,{
-            x : 1,
+    }, [targetsPos, meshRef])
+
+    function animaChild(target, delay) {
+        gsap.to(target.scale, {
+            x: 1,
             y: 1,
-            z : 1,
-           
+            z: 1,
+
             duration: 1,
-            ease:Power2.easeInOut
+            ease: Power2.easeInOut
         })
-        gsap.to(target.material,{
-            opacity : 1,
+        gsap.to(target.material, {
+            opacity: 1,
             duration: 1,
             delay: delay,
-            ease:Power2.easeInOut
-        }) 
+            ease: Power2.easeInOut
+        })
     }
 
     return (
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -4,7 +4,7 @@ import GlobalProduct3D from '.././components/3d/GlobalProduct3D'
 import * as THREE from 'three';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import PlaneGeo from '../components/3d/PlaneGeo';
-import { DirectionalLight } from '@react-three/drei';
+import gsap, { Power2 } from 'gsap';
 import { Vector2 ,Vector3} from 'three';
 import DemoImg from '../components/3d/DemoImg';
 extend({ OrbitControls })
@@ -16,15 +16,34 @@ function Controls() {
  
   gl.gammaFactor = 2.2
   gl.outputEncoding = THREE.sRGBEncoding
-  useEffect(() => {
-    camera.position.set(0,0,500)
 
-    console.log(scene)
+  useEffect(() => {
+    camera.position.set(0,0,300)
   })
+
   useFrame(() => {
-    controls.current.update()
+    controls.current.update();
   })
-  return <orbitControls ref={controls} args={[camera, gl.domElement]} enableDamping dampingFactor={0.1} enableRotate={false}  enableZoom={true}  />
+  
+
+   // Listen to the scroll event and tween the camera position and zoom level
+ /*   useEffect(() => {
+    const handleScroll = (e) => {
+      console.log('run')
+
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      const zoomLevel = Math.max(1 - scrollTop * 0.001, 0.5);
+      const targetPosition = { x: 0, y: 0, z: 300 * zoomLevel };
+      gsap.to(camera.position, { duration: 0.5, ...targetPosition, ease: "power2.out" });
+      controls.current.update();
+    };
+    window.addEventListener("wheel", handleScroll);
+    return () => window.removeEventListener("wheel", handleScroll);
+  }, [camera, controls]); */
+
+
+
+  return <orbitControls ref={controls} args={[camera, gl.domElement]} enableDamping={true} dampingFactor={0.2}zoomSpeed={1.2} enableRotate={false}  enableZoom={true}  />
 }
 
 
@@ -32,21 +51,18 @@ function Controls() {
 
 
 export default function Gallery() {
- 
   return (
     <>
-      <section style={{ backgroundColor: "black", width: "100vw", height: "100vh" }}>
-    
+      <section style={{ backgroundColor: "white", width: "100vw", height: "100vh" }}>
       <Canvas>
    
         <axesHelper args={[500, 500, 500]} />
        {/*  <DemoImg/> */}
         <PlaneGeo/>
-        <Controls />
+        <Controls  />
       </Canvas>
       </section>
 
     </>
-
   )
 }
